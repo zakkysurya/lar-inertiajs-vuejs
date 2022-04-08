eef4bc3fb98627627bc92020bcbc429384e98181
zakkysurya <zakkysurya@gmail.com>
2022-04-08 15:07:35 +0700

    templating page

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,8 +1,117 @@
 <template>
-    <h1>Hello, {{ title }}</h1>
+    <Layout>
+        <!-- Start First Cards -->
+        <!-- *************************************************************** -->
+        <div class="card-group">
+            <div class="card border-right">
+                <div class="card-body">
+                    <div class="d-flex d-lg-flex d-md-block align-items-center">
+                        <div>
+                            <div class="d-inline-flex align-items-center">
+                                <h2 class="text-dark mb-1 font-weight-medium">
+                                    236
+                                </h2>
+                                <span
+                                    class="badge bg-primary font-12 text-white font-weight-medium badge-pill ml-2 d-lg-block d-md-none"
+                                    >+18.33%</span
+                                >
+                            </div>
+                            <h6
+                                class="text-muted font-weight-normal mb-0 w-100 text-truncate"
+                            >
+                                New Clients
+                            </h6>
+                        </div>
+                        <div class="ml-auto mt-md-3 mt-lg-0">
+                            <span class="opacity-7 text-muted"
+                                ><i data-feather="user-plus"></i
+                            ></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card border-right">
+                <div class="card-body">
+                    <div class="d-flex d-lg-flex d-md-block align-items-center">
+                        <div>
+                            <h2
+                                class="text-dark mb-1 w-100 text-truncate font-weight-medium"
+                            >
+                                <sup class="set-doller">$</sup>18,306
+                            </h2>
+                            <h6
+                                class="text-muted font-weight-normal mb-0 w-100 text-truncate"
+                            >
+                                Earnings of Month
+                            </h6>
+                        </div>
+                        <div class="ml-auto mt-md-3 mt-lg-0">
+                            <span class="opacity-7 text-muted"
+                                ><i data-feather="dollar-sign"></i
+                            ></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card border-right">
+                <div class="card-body">
+                    <div class="d-flex d-lg-flex d-md-block align-items-center">
+                        <div>
+                            <div class="d-inline-flex align-items-center">
+                                <h2 class="text-dark mb-1 font-weight-medium">
+                                    1538
+                                </h2>
+                                <span
+                                    class="badge bg-danger font-12 text-white font-weight-medium badge-pill ml-2 d-md-none d-lg-block"
+                                    >-18.33%</span
+                                >
+                            </div>
+                            <h6
+                                class="text-muted font-weight-normal mb-0 w-100 text-truncate"
+                            >
+                                New Projects
+                            </h6>
+                        </div>
+                        <div class="ml-auto mt-md-3 mt-lg-0">
+                            <span class="opacity-7 text-muted"
+                                ><i data-feather="file-plus"></i
+                            ></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex d-lg-flex d-md-block align-items-center">
+                        <div>
+                            <h2 class="text-dark mb-1 font-weight-medium">
+                                864
+                            </h2>
+                            <h6
+                                class="text-muted font-weight-normal mb-0 w-100 text-truncate"
+                            >
+                                Projects
+                            </h6>
+                        </div>
+                        <div class="ml-auto mt-md-3 mt-lg-0">
+                            <span class="opacity-7 text-muted"
+                                ><i data-feather="globe"></i
+                            ></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- *************************************************************** -->
+        <!-- End First Cards -->
+    </Layout>
 </template>
 <script>
+import Layout from "../Layouts/Layout.vue";
+
 export default {
-    props: ["title"],
+    components: {
+        Layout,
+    },
 };
 </script>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,13 +1,84 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+<html dir="ltr" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('admin-mart/src/assets/images/favicon.png')}}">
+    <title>Loading...</title>
+    <!-- Custom CSS -->
+    <link href="{{asset('admin-mart/src/assets/extra-libs/c3/c3.min.css')}}" rel="stylesheet">
+    <link href="{{asset('admin-mart/src/assets/libs/chartist/dist/chartist.min.css')}}" rel="stylesheet">
+    <link href="{{asset('admin-mart/src/assets/extra-libs/jvector/jquery-jvectormap-2.0.2.css')}}" rel="stylesheet" />
+    <!-- Custom CSS -->
+    <link href="{{asset('admin-mart/src/dist/css/style.min.css')}}" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/app.js') }}" defer></script>
     @inertiaHead
-  </head>
-  <body>
-    @inertia
-  </body>
+</head>
+
+<body>
+    <!-- ============================================================== -->
+    <!-- Preloader - style you can find in spinners.css -->
+    <!-- ============================================================== -->
+    <div class="preloader">
+        <div class="lds-ripple">
+            <div class="lds-pos"></div>
+            <div class="lds-pos"></div>
+        </div>
+    </div>
+    <!-- ============================================================== -->
+    <!-- Main wrapper - style you can find in pages.scss -->
+    <!-- ============================================================== -->
+    <div id="main-wrapper" data-theme="light" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+        data-sidebar-position="fixed" data-header-position="fixed" data-boxed-layout="full">
+        <!-- ============================================================== -->
+        <!-- Topbar header - style you can find in pages.scss -->
+        <!-- ============================================================== -->
+        @include('materials.header')
+        <!-- ============================================================== -->
+        <!-- End Topbar header -->
+
+        @inertia
+
+    </div>
+    <!-- ============================================================== -->
+    <!-- End Wrapper -->
+    <!-- ============================================================== -->
+    <!-- End Wrapper -->
+    <!-- ============================================================== -->
+    <!-- All Jquery -->
+    <!-- ============================================================== -->
+    <script src="{{asset('admin-mart/src/assets/libs/jquery/dist/jquery.min.js')}}"></script>
+    <script src="{{asset('admin-mart/src/assets/libs/popper.js/dist/umd/popper.min.js')}}"></script>
+    <script src="{{asset('admin-mart/src/assets/libs/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+    <!-- apps -->
+    <!-- apps -->
+    <script src="{{asset('admin-mart/src/dist/js/app-style-switcher.js')}}"></script>
+    <script src="{{asset('admin-mart/src/dist/js/feather.min.js')}}"></script>
+    <script src="{{asset('admin-mart/src/assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js')}}"></script>
+    <script src="{{asset('admin-mart/src/dist/js/sidebarmenu.js')}}"></script>
+    <!--Custom JavaScript -->
+    <script src="{{asset('admin-mart/src/dist/js/custom.min.js')}}"></script>
+    <!--This page JavaScript -->
+    <script src="{{asset('admin-mart/src/assets/extra-libs/c3/d3.min.js')}}"></script>
+    <script src="{{asset('admin-mart/src/assets/extra-libs/c3/c3.min.js')}}"></script>
+    <script src="{{asset('admin-mart/src/assets/libs/chartist/dist/chartist.min.js')}}"></script>
+    <script src="{{asset('admin-mart/src/assets/libs/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js')}}"></script>
+    <script src="{{asset('admin-mart/src/assets/extra-libs/jvector/jquery-jvectormap-2.0.2.min.js')}}"></script>
+    <script src="{{asset('admin-mart/src/assets/extra-libs/jvector/jquery-jvectormap-world-mill-en.js')}}"></script>
+    <script src="{{asset('admin-mart/src/dist/js/pages/dashboards/dashboard1.min.js')}}"></script>
+</body>
+
 </html>
